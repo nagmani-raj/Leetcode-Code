4eeaf8a81dc1a1b465477f26d2e1022a2bebe200
Nagmani Raj <nagmaniraj133@gmail.com>
2026-04-05 07:21:10 +0530

    Time: 7 ms (30.80%) | Memory: 45.8 MB (51.84%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -3,7 +3,8 @@ class Solution {
         int x = 0; 
         int y = 0; 
         
-        for (char move : moves.toCharArray()) {
+        for (int i = 0; i < moves.length(); i++) {
+            char move = moves.charAt(i);
             if (move == 'U') {
                 y++;
             } else if (move == 'D') {
