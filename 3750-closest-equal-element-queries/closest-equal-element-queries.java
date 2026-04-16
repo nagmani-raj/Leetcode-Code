1d7275a88a59cecbeeae89513fce796333675ede
Nagmani Raj <nagmaniraj133@gmail.com>
2026-04-16 09:07:02 +0530

    Time: 107 ms (61.62%) | Memory: 188 MB (47.47%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int q : queries) {
+            int val = nums[q];
+            List<Integer> list = map.get(val);
+
+            if (list.size() == 1) {
+                ans.add(-1);
+                continue;
+            }
+
+            int pos = Collections.binarySearch(list, q);
+
+            int size = list.size();
+
+            int prev = list.get((pos - 1 + size) % size);
+            int next = list.get((pos + 1) % size);
+
+            int distPrev = Math.min(Math.abs(q - prev), n - Math.abs(q - prev));
+            int distNext = Math.min(Math.abs(q - next), n - Math.abs(q - next));
+
+            ans.add(Math.min(distPrev, distNext));
+        }
+
+        return ans;
+    }
+}
