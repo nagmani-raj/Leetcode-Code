786897cd418126f58dabdb24cd3cb580f3612643
Nagmani Raj <nagmaniraj133@gmail.com>
2026-06-18 20:21:03 +0530

    Time: 5 ms (36.60%) | Memory: 45.4 MB (73.73%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,38 +1,31 @@
 class Solution {
 
-    private int solve(int row, int col,
-                      List<List<Integer>> triangle,
-                      int[][] dp) {
-
-        int n = triangle.size();
-
-        // Base case
-        if (row == n - 1) {
-            return triangle.get(row).get(col);
-        }
-
-        if (dp[row][col] != Integer.MIN_VALUE) {
-            return dp[row][col];
-        }
-
-        int down = solve(row + 1, col, triangle, dp);
-        int diagonal = solve(row + 1, col + 1, triangle, dp);
-
-        return dp[row][col] =
-                triangle.get(row).get(col)
-                + Math.min(down, diagonal);
-    }
-
     public int minimumTotal(List<List<Integer>> triangle) {
 
         int n = triangle.size();
 
         int[][] dp = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        // Last row copy
+        for (int j = 0; j < triangle.get(n - 1).size(); j++) {
+            dp[n - 1][j] = triangle.get(n - 1).get(j);
         }
 
-        return solve(0, 0, triangle, dp);
+        for (int row = n - 2; row >= 0; row--) {
+
+            for (int col = 0;
+                 col < triangle.get(row).size();
+                 col++) {
+
+                int down = dp[row + 1][col];
+                int diagonal = dp[row + 1][col + 1];
+
+                dp[row][col] =
+                        triangle.get(row).get(col)
+                        + Math.min(down, diagonal);
+            }
+        }
+
+        return dp[0][0];
     }
 }
