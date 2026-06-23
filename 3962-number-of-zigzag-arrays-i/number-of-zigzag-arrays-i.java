6024f98ea18f8b442257a02338a6eb3590a06cb0
Nagmani Raj <nagmaniraj133@gmail.com>
2026-06-23 09:46:12 +0530

    Time: 202 ms (92.50%) | Memory: 44.7 MB (92.50%) - LeetSync

---
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int zigZagArrays(int n, int l, int r) {
+        int mod = (int) (1e9 + 7);
+        r -= l;
+        long[] prefix = new long[r];
+        Arrays.fill(prefix, 1);
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] += prefix[i - 1];
+        }
+        boolean zig = true;
+        for (int i = 1; i < n - 1; i++) {
+            if (zig) {
+                for (int j = prefix.length - 2; j >= 0; j--) {
+                    prefix[j] += prefix[j + 1];
+                    prefix[j] %= mod;
+                }
+            } else {
+                for (int j = 1; j < prefix.length; j++) {
+                    prefix[j] += prefix[j - 1];
+                    prefix[j] %= mod;
+                }
+            }
+            zig = !zig;
+        }
+        long result = 0;
+        for (long p : prefix) {
+            result += p;
+            result %= mod;
+        }
+        result *= 2;
+        result %= mod;
+        return (int) result;
+    }
+}
