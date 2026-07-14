9b72ab4cadea9e457b0f657e38e26e48e73f3ddb
Nagmani Raj <nagmaniraj133@gmail.com>
2026-07-14 16:31:55 +0530

    Time: 1 ms (99.99%) | Memory: 43.1 MB (72.55%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestPalindrome(String s) {
+
+        int[] freq = new int[128];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch]++;
+        }
+
+        int ans = 0;
+        boolean odd = false;
+
+        for (int count : freq) {
+            if (count % 2 == 0) {
+                ans += count;
+            } else {
+                ans += count - 1;
+                odd = true;
+            }
+        }
+
+        if (odd) ans++;
+
+        return ans;
+    }
+}
