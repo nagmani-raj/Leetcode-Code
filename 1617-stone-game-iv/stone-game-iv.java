278128ba1ef23239ff1b56040846ff4c106b2ad5
Nagmani Raj <nagmaniraj133@gmail.com>
2026-08-10 06:37:15 +0530

    Time: 14 ms (91.38%) | Memory: 42.4 MB (78.33%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int k = 1; k * k <= i; k++) {
+                if (!dp[i - k * k]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
