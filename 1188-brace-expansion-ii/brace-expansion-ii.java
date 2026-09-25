d7546ffad8442599e59243d0fd4b5b6768608bb3
Nagmani Raj <nagmaniraj133@gmail.com>
2026-09-25 10:09:12 +0530

    Time: 11 ms (47.20%) | Memory: 47.5 MB (36.80%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.java
@@ -0,0 +1,67 @@
+public class Solution {
+    public List<String> braceExpansionII(String expression) {
+        Set<String> res = flatten(expression);
+        List<String> sorted = new ArrayList<>(res);
+        Collections.sort(sorted);
+        return sorted;
+    }
+
+    private Set<String> flatten(String expression) {
+        Set<String> res = new HashSet<>();
+        // A temp set to store cartesian product results.
+        Set<String> curSet = new HashSet<>();
+        int idx = 0;
+        while (idx < expression.length()) {
+            if (expression.charAt(idx) == '{') {
+                // end will be the index of matching "}"
+                int end = findClosingBrace(expression, idx);
+                Set<String> set = flatten(expression.substring(idx + 1, end));
+                curSet = concatenateSet(curSet, set);
+                idx = end + 1;
+            } else if (Character.isLowerCase(expression.charAt(idx))) {
+                // Create set with single element
+                Set<String> set =
+                        new HashSet<>(Arrays.asList(Character.toString(expression.charAt(idx))));
+                curSet = concatenateSet(curSet, set);
+                idx++;
+            } else if (expression.charAt(idx) == ',') {
+                res.addAll(curSet);
+                curSet.clear();
+                idx++;
+            }
+        }
+        // Don't forget!
+        res.addAll(curSet);
+        return res;
+    }
+
+    private Set<String> concatenateSet(Set<String> set1, Set<String> set2) {
+        if (set1.isEmpty() || set2.isEmpty()) {
+            return !set2.isEmpty() ? new HashSet<>(set2) : new HashSet<>(set1);
+        }
+        Set<String> res = new HashSet<>();
+        for (String s1 : set1) {
+            for (String s2 : set2) {
+                res.add(s1 + s2);
+            }
+        }
+        return res;
+    }
+
+    private int findClosingBrace(String expression, int start) {
+        int count = 0;
+        int idx = start;
+        while (idx < expression.length()) {
+            if (expression.charAt(idx) == '{') {
+                count++;
+            } else if (expression.charAt(idx) == '}') {
+                count--;
+            }
+            if (count == 0) {
+                break;
+            }
+            idx++;
+        }
+        return idx;
+    }
+}
